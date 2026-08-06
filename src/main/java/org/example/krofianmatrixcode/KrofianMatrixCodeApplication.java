7e8f84585b6fa72eb579d8fc532ebbc07509d71e
makala0 <ejmacala@gmail.com>
2026-08-06 09:08:57 +0200

    cz to en

--- src/main/java/org/example/krofianmatrixcode/KrofianMatrixCodeApplication.java
+++ src/main/java/org/example/krofianmatrixcode/KrofianMatrixCodeApplication.java
@@ -3,11 +3,25 @@ package org.example.krofianmatrixcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.swing.*;
+
 @SpringBootApplication
 public class KrofianMatrixCodeApplication {
 
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            // Create a simple GUI frame
+            JFrame frame = new JFrame("AMViewer");
+            JLabel label = new JLabel("Application is running!", SwingConstants.CENTER);
+            frame.add(label);
+
+            // Configure the frame
+            frame.setSize(300, 150);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setVisible(true);
+        });
         SpringApplication.run(KrofianMatrixCodeApplication.class, args);
     }
-
 }
+
+
